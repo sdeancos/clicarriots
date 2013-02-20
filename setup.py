68672ffced3c862a8df4b4ee0589f2aced1dded7
Samuel <samuel@ss.(none)>
2013-02-20 14:19:47 +0100

    Package

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='clicarriots',
       version='0.1',
-      py_modules=['clicarriots'],
+      author='Samuel de Ancos',
+      author_email='sdeancos@gmail.com',
+      packages=['clicarriots'],
       )
