7706ca346f3af0a011733c89f128e7aca9ca64e7
Samuel de Ancos <sdeancos@gmail.com>
2016-01-20 17:04:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 setup(name='clicarriots',
       version='0.1',
+      description = 'The unofficial simple client for carriots platform (in progress) (python 2.x/3.x)',
       author='Samuel de Ancos',
       author_email='sdeancos@gmail.com',
+      url = 'https://github.com/sdeancos/clicarriotas',
+      download_url = 'https://github.com/sdeancos/clicarriotas/tarball/0.1',
       packages=['clicarriots'],
       )
